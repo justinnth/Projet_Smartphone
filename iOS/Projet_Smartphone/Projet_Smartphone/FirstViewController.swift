378985af53435e0387a02f7dfa2ac5263f637b75
Justin North <justin_n@hotmail.fr>
2019-03-24 18:10:52 +0100

    iOS Start
    Initialisation du projet Xcode

---
+++ iOS/Projet_Smartphone/Projet_Smartphone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Projet_Smartphone
+//
+//  Created by Justin North on 24/03/2019.
+//  Copyright © 2019 Justin North. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
