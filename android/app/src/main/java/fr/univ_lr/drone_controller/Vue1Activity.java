9bd7eb52154f7463f1766408c9694a72c27a8c84
Flo <26801813+BananaPaper@users.noreply.github.com>
2019-03-26 14:17:27 +0100

    Fonction parcours de fichier implémentée

--- android/app/src/main/java/fr/univ_lr/drone_controller/Vue1Activity.java
+++ android/app/src/main/java/fr/univ_lr/drone_controller/Vue1Activity.java
@@ -3,11 +3,14 @@ package fr.univ_lr.drone_controller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
+
+import java.io.*;
 
 public class Vue1Activity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -47,11 +50,29 @@ public class Vue1Activity extends AppCompatActivity implements OnMapReadyCallbac
                 finish();
             }
         });
+        Log.w("ok","ok");
+        nmeaParse("tramesNMEA.txt");
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
         LatLng loc = new LatLng(46.1425159,-1.1444612);
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(loc,13));
+    }
+
+    public void nmeaParse(String fileName) {
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open(fileName)));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Log.i("FILEREADER",line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
